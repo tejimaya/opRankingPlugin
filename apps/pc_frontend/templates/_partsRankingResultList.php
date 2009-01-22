05363a30ffcea9d938be442c6758e4e022923f05
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-22 11:45:37 +0000

    #3468:fixed The variable name was changed into options from option
    git-svn-id: https://svn.openpne.jp/svn/OpenPNE3/plugins/opRankingPlugin/trunk@10319 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/templates/_partsRankingResultList.php
+++ apps/pc_frontend/templates/_partsRankingResultList.php
@@ -1,10 +1,10 @@
-<?php foreach ($option['model'] as $key => $model): ?>
+<?php foreach ($options['model'] as $key => $model): ?>
 
-<?php if (!$key || ($key > 0 && $option['rank'][$key] != $option['rank'][$key - 1])): ?>
-<div class="dparts rankingResultList <?php ($option['rank'][$key] == 1) ? print('rank_top') : print('') ?>"><div class="ranking_parts">
-<?php if ($option['rank'][$key] == 1): ?>
+<?php if (!$key || ($key > 0 && $options['rank'][$key] != $options['rank'][$key - 1])): ?>
+<div class="dparts rankingResultList <?php ($options['rank'][$key] == 1) ? print('rank_top') : print('') ?>"><div class="ranking_parts">
+<?php if ($options['rank'][$key] == 1): ?>
 <div class="partsHeading">
-<h3><?php echo $option['title'] ?></h3>
+<h3><?php echo $options['title'] ?></h3>
 </div>
 <?php else: ?>
 <div class="ditem">
@@ -15,23 +15,23 @@
 <?php endif; ?>
 
 <tr>
-<td rowspan="<?php echo count($option['list'][$key]) + 1 ?>" class="photo">
-<?php echo link_to(image_tag_sf_image($model->getImageFilename(), array('size' => '76x76')), sprintf($option['link_to_detail'], $model->getId())); ?>
+<td rowspan="<?php echo count($options['list'][$key]) + 1 ?>" class="photo">
+<?php echo link_to(image_tag_sf_image($model->getImageFilename(), array('size' => '76x76')), sprintf($options['link_to_detail'], $model->getId())); ?>
 </td>
 </tr>
 
-<?php foreach ($option['list'][$key] as $caption => $item) : ?>
+<?php foreach ($options['list'][$key] as $caption => $item) : ?>
 <tr>
 <th><?php echo $caption ?></th>
 <td><?php echo $item ?></td>
 </tr>
 <?php endforeach; ?>
 
-<?php if ($key == count($option['rank']) - 1 || $option['rank'][$key] != $option['rank'][$key + 1]): ?>
+<?php if ($key == count($options['rank']) - 1 || $options['rank'][$key] != $options['rank'][$key + 1]): ?>
 </tbody>
 </table>
 </div>
-<?php if ($option['rank'][$key] != 1): ?>
+<?php if ($options['rank'][$key] != 1): ?>
 </div>
 <?php endif; ?>
 </div>
